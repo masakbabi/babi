b2f36b90b6062aa9ed67ce9d2835721f5fc3a87b
masakbabi <149098260+masakbabi@users.noreply.github.com>
2023-10-27 01:00:05 +0700

    Update moonton.py

--- moonton.py
+++ moonton.py
@@ -48,7 +48,7 @@ except ImportError:
     'Please restart this tools'
   )
 
-api = 'https://mtacc.mobilelegends.com/v2.1/inapp/login/'
+api = 'https://accountmtapi.mobilelegends.com/'
 
 class MOONTON:
   def __init__(self, url):
